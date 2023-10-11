2553cb918790dcf6f6ea8999f86aa230d932d4ae
pepe <blanco@blanco.com>
2023-10-11 12:50:45 +0200

    arbol de directorios

--- src/main/java/n1exercici1/Main.java
+++ src/main/java/n1exercici1/Main.java
@@ -1,4 +1,15 @@
 package n1exercici1;
 
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
 }
